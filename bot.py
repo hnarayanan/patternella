f4ca1e8fec28d069d026892122fa3c97afa9f311
Harish Narayanan <hnarayanan@gmail.com>
2017-11-26 22:20:12 +0000

    Fix many more small bugs and improve exception handling

--- bot.py
+++ bot.py
@@ -13,21 +13,30 @@ class PatternellaStreamListener(tweepy.StreamListener):
     def on_status(self, status):
         sender = status.author.screen_name
         try:
+            photo_id = status.extended_entities['media'][0]['id_str']
             photo_url = status.extended_entities['media'][0]['media_url']
             photo = Image.open(urllib2.urlopen(photo_url))
-            photo.save('random.jpg')
+            photo.save('input/{photo_id}.png'.format(photo_id=photo_id))
+
+            # TODO: We need to grab the style from the style hashtag if it exists
             # style = status.entities['hashtags'][0]
+
             tweet = "@{sender} Here you go!".format(sender=sender)
-            api.update_with_media('/data/projects/patternella/random.jpg', status=tweet)
+            api.update_with_media('input/{photo_id}.png'.format(photo_id=photo_id), status=tweet)
         except:
             tweet = "@{sender} Please tweet me with the image you'd like cutened!".format(sender=sender)
-            api.update_status(status=tweet)
+            try:
+                api.update_status(status=tweet)
+            except tweepy.error.TweepError:
+                # TODO: This exception should be better logged, e.g.
+                # print("Error: ({code}): {message}".format(code=error['code'], message=error['message']))
+                pass
         print tweet
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)        
+api = tweepy.API(auth)
 
 listener = PatternellaStreamListener()
 stream = tweepy.Stream(auth=api.auth, listener=listener)
