200952954cd528e3d183396678b149688f799d67
Harish Narayanan <hnarayanan@gmail.com>
2017-11-26 19:08:49 +0000

    Use the stream API to gather @replies to the bot

--- bot.py
+++ bot.py
@@ -7,5 +7,15 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-tweet = 'Hello, from the patternella bot!'
-api.update_status(status=tweet)
+class PatternellaStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status)
+        # api.update_with_media('/data/projects/tensorflow-fast-style-transfer/model/result_15000.jpg', status='Amazing!')
+        print(status.text)
+
+
+listener = PatternellaStreamListener()
+stream = tweepy.Stream(auth=api.auth, listener=listener)
+
+stream.filter(track=['@patternella'])
