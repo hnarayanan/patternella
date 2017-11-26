69dd4f0b2528d3e6dc5178ee77e55b7b817ba6a6
Harish Narayanan <hnarayanan@gmail.com>
2017-11-26 23:06:45 +0000

    Commit WIP in actually transforming the incoming photo

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import tweepy
 import urllib2
 from PIL import Image
 
+import tensorflow as tf
+from style_transfer_tester import StyleTransferTester
+
 from credentials import (consumer_key,
                          consumer_secret,
                          access_token,
@@ -9,6 +12,10 @@ from credentials import (consumer_key,
 
 
 class PatternellaStreamListener(tweepy.StreamListener):
+
+    def __init__(self, api, sess):
+        self.sess = sess
+        self.api = api
 
     def on_status(self, status):
         sender = status.author.screen_name
@@ -20,13 +27,22 @@ class PatternellaStreamListener(tweepy.StreamListener):
 
             # TODO: We need to grab the style from the style hashtag if it exists
             # style = status.entities['hashtags'][0]
+            style = 'la_muse'
+
+            transformer = StyleTransferTester(
+                session=self.sess,
+                model_path='style_models/{style}.ckpt'.format(style=style),
+                content_image=photo,
+            )
+            transformed_photo = transformer.test()
+            photo.save('output/{photo_id}.png'.format(photo_id=photo_id))
 
             tweet = "@{sender} Here you go!".format(sender=sender)
-            api.update_with_media('input/{photo_id}.png'.format(photo_id=photo_id), status=tweet)
+            self.api.update_with_media('output/{photo_id}.png'.format(photo_id=photo_id), status=tweet)
         except:
             tweet = "@{sender} Please tweet me with the image you'd like cutened!".format(sender=sender)
             try:
-                api.update_status(status=tweet)
+                self.api.update_status(status=tweet)
             except tweepy.error.TweepError:
                 # TODO: This exception should be better logged, e.g.
                 # print("Error: ({code}): {message}".format(code=error['code'], message=error['message']))
@@ -38,7 +54,11 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-listener = PatternellaStreamListener()
+soft_config = tf.ConfigProto(allow_soft_placement=True)
+soft_config.gpu_options.allow_growth = True
+sess = tf.Session(config=soft_config)
+
+listener = PatternellaStreamListener(sess=sess, api=api)
 stream = tweepy.Stream(auth=api.auth, listener=listener)
 
 stream.filter(track=['@patternella'])
