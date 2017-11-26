8e01699ef7d6a018e1ef9e6f7b55a88af85ae7d8
Harish Narayanan <hnarayanan@gmail.com>
2017-11-26 20:16:25 +0000

    Commit skeletal bot that responds to images (vs not)

--- bot.py
+++ bot.py
@@ -1,19 +1,33 @@
 import tweepy
+import urllib2
 
-from credentials import *
+from credentials import (consumer_key,
+                         consumer_secret,
+                         access_token,
+                         access_token_secret)
 
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
 
 class PatternellaStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        print(status)
-        # api.update_with_media('/data/projects/tensorflow-fast-style-transfer/model/result_15000.jpg', status='Amazing!')
-        print(status.text)
+        sender = status.user.screen_name
+        try:
+            print "I got an image"
+            photo_url = status.extended_entities['media'][0]['media_url']
+            photo = Image.open(urllib2.urlopen(photo_url))
+            photo.save('random.jpg')
+            # style = status.entities['hashtags'][0]
+            tweet = "@{sender} Here you go!".format(sender=sender)
+            api.update_with_media('/data/projects/tensorflow-fast-style-transfer/model/result_15000.jpg', status=tweet)
+        except:
+            print "I didn't get an image"
+            tweet = "@{sender} Please tweet me with the image you'd like cutened!".format(sender=sender)
+            api.update_status(status=tweet)
 
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)        
 
 listener = PatternellaStreamListener()
 stream = tweepy.Stream(auth=api.auth, listener=listener)
